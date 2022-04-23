37e470ce478abb242e6d6e6962f91bc70332b346
Palash Dusane <palashdusane@gmail.com>
2022-04-23 20:05:18 +0530

    Added expect check in spec files

--- calculatorSpec.ts
+++ calculatorSpec.ts
@@ -1,7 +1,8 @@
-import { browser } from "protractor"
+import { browser, by, element } from "protractor"
 
 describe('Calculate operations', function(){
     it('Open calculator demo website', function(){
         browser.get('http://juliemr.github.io/protractor-demo/');
+        expect(browser.getTitle()).toEqual('Super Calculator');
     });
 });
--- testspec.ts
+++ testspec.ts
@@ -4,5 +4,6 @@ describe('chain locators demo',function(){
 
     it('open protractor official website',function(){
         browser.get('https://www.protractortest.org/#/');
+        expect(browser.getTitle()).toEqual('Protractor - end-to-end testing for AngularJS');
     });
 });
