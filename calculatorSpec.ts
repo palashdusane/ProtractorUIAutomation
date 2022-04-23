15ec01c952aa02a74f9ce00d0f5451d3b2bd93ea
Palash Dusane <palashdusane@gmail.com>
2022-04-23 21:44:27 +0530

    Added logic to add 2 nos in calculation spec file

--- calculatorSpec.ts
+++ calculatorSpec.ts
@@ -1,8 +1,13 @@
-import { browser } from "protractor";
+import { browser, by, element } from "protractor";
 
 describe('Calculate operations', async()=>{
     it('Open calculator demo website', async()=>{
         await browser.get('http://juliemr.github.io/protractor-demo/');
         await expect(browser.getTitle()).toEqual('Super Calculator');
+
+        await element(by.model('first')).sendKeys(3);
+        await element(by.model('second')).sendKeys(3);
+        await element(by.id('gobutton')).click();       
+        await expect(element(by.xpath('//h2[@class ="ng-binding"]')).getText()).toEqual('6');
     });
 });
