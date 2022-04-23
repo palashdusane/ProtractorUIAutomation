7c78c38f2cb2f97cf143396ccf5954c55bf66f43
Palash Dusane <palashdusane@gmail.com>
2022-04-23 20:49:07 +0530

    Added await in test spec file

--- testspec.ts
+++ testspec.ts
@@ -2,7 +2,7 @@ import { browser } from "protractor";
 
 describe('chain locators demo',async()=>{
     it('open protractor official website',async()=>{
-        browser.get('https://www.protractortest.org/#/');
-        expect(browser.getTitle()).toEqual('Protractor - end-to-end testing for AngularJS');
+        await browser.get('https://www.protractortest.org/#/');
+        await expect(browser.getTitle()).toEqual('Protractor - end-to-end testing for AngularJS');
     });
 });
