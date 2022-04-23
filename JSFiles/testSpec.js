c4ce24c429f493b62a6a40d6a384a71612f322ac
Palash Dusane <palashdusane@gmail.com>
2022-04-23 19:09:59 +0530

    Added git ignore file

---
+++ JSFiles/testSpec.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const protractor_1 = require("protractor");
+describe('chain locators demo', function () {
+    it('open protractor official website', function () {
+        protractor_1.browser.get('https://www.protractortest.org/#/');
+    });
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGVzdFNwZWMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi90ZXN0U3BlYy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBLDJDQUFvQztBQUVwQyxRQUFRLENBQUMscUJBQXFCLEVBQUM7SUFFM0IsRUFBRSxDQUFDLGtDQUFrQyxFQUFDO1FBQ2xDLG9CQUFPLENBQUMsR0FBRyxDQUFDLG1DQUFtQyxDQUFDLENBQUM7SUFDckQsQ0FBQyxDQUFDLENBQUM7QUFDUCxDQUFDLENBQUMsQ0FBQyJ9
--- JSFiles/testspec.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const protractor_1 = require("protractor");
-describe('chain locators demo', function () {
-    it('open protractor official website', function () {
-        protractor_1.browser.get('https://www.protractortest.org/#/');
-    });
-});
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGVzdHNwZWMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi90ZXN0c3BlYy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBLDJDQUFvQztBQUVwQyxRQUFRLENBQUMscUJBQXFCLEVBQUM7SUFFM0IsRUFBRSxDQUFDLGtDQUFrQyxFQUFDO1FBQ2xDLG9CQUFPLENBQUMsR0FBRyxDQUFDLG1DQUFtQyxDQUFDLENBQUM7SUFDckQsQ0FBQyxDQUFDLENBQUM7QUFDUCxDQUFDLENBQUMsQ0FBQyJ9
