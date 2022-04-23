45637153b104586c7aaf37a43b20c4e64cd36407
Palash Dusane <palashdusane@gmail.com>
2022-04-23 20:32:22 +0530

    syntax correction

--- calculatorSpec.ts
+++ calculatorSpec.ts
@@ -1,4 +1,4 @@
-import { browser, by, element } from "protractor"
+import { browser } from "protractor";
 
 describe('Calculate operations', function(){
     it('Open calculator demo website', function(){
