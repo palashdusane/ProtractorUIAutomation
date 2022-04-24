23d04a67dbaa9fc81f2197791ac83c717feeeb61
Palash Dusane <palashdusane@gmail.com>
2022-04-24 12:22:07 +0530

    Optimized test cases

--- calculatorSpec.ts
+++ calculatorSpec.ts
@@ -4,10 +4,12 @@ describe('Calculate operations', async()=>{
     it('Open calculator demo website', async()=>{
         await browser.get('http://juliemr.github.io/protractor-demo/');
         await expect(browser.getTitle()).toEqual('Super Calculator');
+    });
 
+    it('Add two numbers', async()=>{
         await element(by.model('first')).sendKeys(3);
         await element(by.model('second')).sendKeys(3);
         await element(by.id('gobutton')).click();       
         await expect(element(by.xpath('//h2[@class ="ng-binding"]')).getText()).toEqual('6');
-    });
+    })
 });
--- testspec.ts
+++ testspec.ts
@@ -1,8 +1,14 @@
-import { browser } from "protractor";
+import { browser, by, element } from "protractor";
 
 describe('chain locators demo',async()=>{
     it('open protractor official website',async()=>{
         await browser.get('https://www.protractortest.org/#/');
         await expect(browser.getTitle()).toEqual('Protractor - end-to-end testing for AngularJS');
+    });
+
+    it('Navigate the tutorial page',async()=>{
+        await element(by.linkText('Quick Start')).click();
+        await element(by.linkText('Tutorial')).click();
+        await expect(element(by.tagName('h1')).getText()).toEqual('Tutorial');
     });
 });
